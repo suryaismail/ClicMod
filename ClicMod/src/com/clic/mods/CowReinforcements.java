2c74c8d05af76ba6d2bc8425289d5aadb5888bf2
CookieLover <AlimiAndNur@suryaissmacbook.lan>
2017-03-23 14:35:17 +0800

    This is my version of CreeperReinforcements. Instead of creeper reinforcements, its cows. Don't kill cows. They will come back for you.

--- ClicMod/src/com/clic/mods/CowReinforcements.java
+++ ClicMod/src/com/clic/mods/CowReinforcements.java
@@ -1,15 +1,49 @@
 package com.clic.mods;
 
-import net.minecraft.entity.monster.EntityIronGolem;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class CowReinforcements {
 
 	@SubscribeEvent
-	public void eventLALA(LivingUpdateEvent event) {
-		// TODO
-	}
+	public void spawnReinforcements(LivingDeathEvent event){
+		if (!(event.getEntity() instanceof EntityCow)) {
+			return;
+		}
 
+		for (int i = 0 ; i < 3 ; i++) {
+			EntityZombie zombie = new EntityZombie(event.getEntity().worldObj);
+			zombie.setLocationAndAngles(event.getEntity().posX,
+					event.getEntity().posY,
+					event.getEntity().posZ,
+					0,
+					0);
+			if (!event.getEntity().worldObj.isRemote) {
+				event.getEntity().worldObj.spawnEntityInWorld(zombie);
+			}
+		}
+		for (int s = 0 ; s < 2 ; s++) {
+			EntitySkeleton skeleton = new EntitySkeleton(event.getEntity().worldObj);
+			skeleton.setLocationAndAngles(event.getEntity().posX,
+					event.getEntity().posY,
+					event.getEntity().posZ,
+					0,
+					0);
+			skeleton.setHeldItem(EnumHand.MAIN_HAND, new ItemStack(Items.BOW));
+			skeleton.setItemStackToSlot(EntityEquipmentSlot.HEAD, new ItemStack(Items.IRON_HELMET));
+			skeleton.setItemStackToSlot(EntityEquipmentSlot.CHEST, new ItemStack(Items.IRON_CHESTPLATE));
+			skeleton.setItemStackToSlot(EntityEquipmentSlot.LEGS, new ItemStack(Items.IRON_LEGGINGS));
+			skeleton.setItemStackToSlot(EntityEquipmentSlot.FEET, new ItemStack(Items.IRON_BOOTS));
+			if (!event.getEntity().worldObj.isRemote) {
+				event.getEntity().worldObj.spawnEntityInWorld(skeleton);
+			}
+		}
+	}
 }
