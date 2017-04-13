1188f4b1851bd8adb296ea840d723eb328ca5d46
quahxuanyu <quahxuanyu@LAPTOP-JITFP0V6>
2017-04-13 15:09:26 +0800

    chainmail guy

---
+++ ClicMod/src/com/clic/mods/PlayerExplode.java
@@ -0,0 +1,36 @@
+package com.clic.mods;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
+
+public class PlayerExplode {
+
+	static boolean hasExploded = false;
+
+	@SubscribeEvent
+	public void blowupplayer(PlayerTickEvent event) {
+		if (hasExploded) {
+			return;
+		}
+		EntityPlayer player = event.player;
+		ItemStack item = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
+		if (!item.isItemEqual(new ItemStack(Items.CHAINMAIL_CHESTPLATE))) {
+			hasExploded = false;
+			return;
+		}
+		player.getEntityWorld().createExplosion(null,
+				player.getPosition().getX(),
+				player.getPosition().getY(),
+				player.getPosition().getZ(), 10, true);
+		hasExploded = true;
+
+
+
+	}
+}
+
+
