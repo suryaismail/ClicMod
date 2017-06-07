8512b28984bd6d99e57953adb781f770a5a60bbd
suryaismail <suryaismail@yahoo.com>
2017-06-08 06:45:29 +0800

    Update Main.java
    Comment out block break

--- ClicMod/src/com/clic/mods/Main.java
+++ ClicMod/src/com/clic/mods/Main.java
@@ -23,7 +23,7 @@ public class Main {
 	public void init(FMLInitializationEvent event)
 	{
 		
-		MinecraftForge.EVENT_BUS.register(new BlockBreakMessage());
+		// MinecraftForge.EVENT_BUS.register(new BlockBreakMessage());
 		MinecraftForge.EVENT_BUS.register(new PlayerWallClimb());
 		
 		// YunZhe
